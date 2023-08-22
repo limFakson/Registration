7624a0408099580fe266a2e30745f8065c751903
limFakson <fakeyejoshua2005@gmail.com>
2023-08-22 19:18:43 +0100

    lag

--- index.js
+++ index.js
@@ -1,30 +1,50 @@
 document.getElementById('loginLink').addEventListener('click', () => {
     window.location.href = 'next.html';
 });
-const Rform = document.querySelector('#registerform')
-const Lform = document.getElementById('loginform')
+const Rform = document.getElementById('registerform');
+const Lform = document.getElementById('loginform');
+const message = document.getElementById('message');
+const existingUsers = JSON.parse(localStorage.getItem('users')) || [];
 
 
-Rform.addEventListener('submit', (e) => {
-    const name = document.getElementById("name").value
-    const email = document.getElementById("email").value
-    const username = document.getElementById("username").value
-    const password = document.getElementById("password").value
-        //localstorage
+Rform.addEventListener('submit', function(e) {
+    //prevent emaildefault
+    e.preventDefault();
 
-    localStorage.setItem('name', name);
-    localStorage.setItem('username', username);
-    localStorage.setItem('email', email);
-    localStorage.setItem('password', password);
+    const name = document.getElementById("name").value;
+    const email = document.getElementById("email").value;
+    const username = document.getElementById("username").value;
+    const password = document.getElementById("password").value;
 
-    //getItems from localstorage
-    const Remail = localStorage.getItem('email');
-    const Rpassword = localStorage.getItem('password');
+    if (password.length < 6) {
+        message.textContent = "password must be nothing less than 6"
+    } else {
+        message.textContent = ""
+    }
+    //localstorage
+    //creating user object
+    const user = {
+        email: email,
+        name: name,
+        password: password,
+        username: username
+    };
+    //overwrite user
+    existingUsers.push(user);
+
+    //set items inlocalstorage
+    localStorage.setItem('user', JSON.stringify(existingUsers));
+
+    //reload page
+    window.location.href = "index.html"
+
 });
 
 
-Lform.addEventListener('submit', () => {
-    preventdefault()
+Lform.addEventListener('submit', function(e) {
+    //prevent emailmdefault
+    e.preventdefault();
+
     const Lemail = document.getElementById('Loginemail')
     const Lpassword = document.getElementById('Loginpassword')
 
