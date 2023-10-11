cb1fab6dd3273a27986c97867eef3a341bd8a266
limFakson <fakeyejoshua2005@gmail.com>
2023-10-11 21:40:59 +0100

    commit

--- src/profile.js
+++ src/profile.js
@@ -3,6 +3,6 @@ const data = JSON.parse(localStorage.getItem('user'));
 const username = data.username;
 
 
-document.getElementById('demo').textContent = "Hello" + " " + username;
+document.getElementById('demo').textContent = "Hello" + " " + 'username';
 
-console.log(username)
+console.log(username);
