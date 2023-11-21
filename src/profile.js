5b1f4062086a66f89fbb54e4bc6730db7ff2a76b
limFakson <fakeyejoshua2005@gmail.com>
2023-11-21 07:46:33 -0800

    reg

--- src/profile.js
+++ src/profile.js
@@ -1,8 +1,8 @@
 
 const data = JSON.parse(localStorage.getItem('user'));
-const username = data.username;
+const username = data[0].username;
 
 
-document.getElementById('demo').textContent = "Hello" + " " + 'username';
+document.getElementById('demo').textContent = "Hello" + " " + username;
 
 console.log(username);
